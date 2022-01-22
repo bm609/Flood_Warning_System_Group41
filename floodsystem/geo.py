bba0affcbd2adcc0bb79cf520c834d351154b3c5
bm609 <bm609@cam.ac.uk>
2022-01-22 12:03:46 +0000

    Added stations_by_distance function in geo, created Task1B.py test file, added Task1B to the automated testing

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,5 +5,13 @@
 geographical data.
 
 """
-
+from haversine import haversine, Unit #importing haversine library for distance calculations
 from .utils import sorted_by_key  # noqa
+
+def stations_by_distance(stations, p):
+    station_dist = []
+    for i in range(len(stations)):
+        station_dist.append((stations[i].name, stations[i].town, haversine(stations[i].coord, p))) #add current iterations station name, town, and distance. haversine default unit is km
+    station_dist.sort(key = lambda x: x[2]) #sorts list by third element (the distance) represented by x[2]
+    return station_dist
+    
