b10ec4281ea154620cd1c642211efc981557127e
bm609 <bm609@cam.ac.uk>
2022-01-23 11:45:27 +0000

    Completed Task1D, added to automated testing. Deleted some redundant code for Task1B

--- Task1D.py
+++ Task1D.py
@@ -1,8 +1,14 @@
 from floodsystem.geo import rivers_with_station
 from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_river
 
 def run():
     stations = build_station_list()
     test = rivers_with_station(stations)
-    print(test)
+    print(len(test))    #prints length of test list
+    print(test[:10])    #prints first 10 values in test
+    statriver = stations_by_river(stations)
+    print(sorted(statriver["River Aire"]))     #print sorted values corresponding to "River Aire" key
+    print(sorted(statriver["River Cam"]))
+    print(sorted(statriver["River Thames"]))
 run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -10,13 +10,25 @@ from .utils import sorted_by_key  # noqa
 
 def stations_by_distance(stations, p):
     station_dist = []   #initialise list
-    for i in range(len(stations)):
-        station_dist.append((stations[i].name, stations[i].town, haversine(stations[i].coord, p))) #add current iterations station name, town, and distance. haversine default unit is km
+    for i in stations:
+        station_dist.append(i.name, i.town, haversine(i.coord, p)) #add current iterations station name, town, and distance. haversine default unit is km
     station_dist.sort(key = lambda x: x[2]) #sorts list by third element (the distance) represented by x[2]
     return station_dist
     
 def rivers_with_station(stations):
     riverstation = set()   #initialise set
-    for i in range(len(stations)):
-        riverstation.add(stations[i].river) #adds river name to set for every station
+    for i in stations:
+        riverstation.add(i.river) #adds river name to set for every station
+    riverlist = sorted(riverstation)  #converts set into sorted list (alphabetical) for clarity
+    return riverlist
+
+def stations_by_river(stations):
+    riverstation = {}       #riverstation dict
+    rivers = rivers_with_station(stations) #creates list of rivers
+    for i in rivers:            #creates a key of the river's name for each river in rivers
+        riverstation[i] = []    #empty list given as value, so that append may be used
+    for i in stations:
+        riverstation[i.river].append(i.name) #appends station name to respective river for every station
     return riverstation
+
+    
