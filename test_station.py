799e61a16cba09c57bba0e8eac4a9150e888aae4
bm609 <bm609@cam.ac.uk>
2022-02-08 15:27:53 +0000

    added test for inconsistent_typical_range_stations

--- test_station.py
+++ test_station.py
@@ -3,7 +3,8 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
+from floodsystem.stationdata import build_station_list
 
 def test_create_monitoring_station():
 
@@ -25,7 +26,7 @@ def test_create_monitoring_station():
     assert s.river == river
     assert s.town == town
 
-def test_typical_method_consistent():
+def test_typical_range_consistent():
     test1 = MonitoringStation("","","","",None,"","")
     test2 = MonitoringStation("","","","",[2.5,1.5],"","")
     test3 = MonitoringStation("","","","",[1.5,2.5],"","")
@@ -33,3 +34,9 @@ def test_typical_method_consistent():
     assert test1.typical_range_consistent() == False #tests case of typical range = none
     assert test2.typical_range_consistent() == False #tests case of lower range > upper range
     assert test3.typical_range_consistent() == True #tests case of upper range > lower range
+
+def test_inconsistent_typical_range_stations():
+    stations = build_station_list()
+    incon = inconsistent_typical_range_stations(stations)
+    for i in incon:
+        assert i.typical_range_consistent() == False
