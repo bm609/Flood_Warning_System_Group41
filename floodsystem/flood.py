7d74d6562cd3ddbce1b2a0ec200fa8d1460a4569
bm609 <bm609@cam.ac.uk>
2022-02-13 23:10:09 +0000

    Completed Task2C, added automated testing for it

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -10,3 +10,17 @@ def stations_level_over_threshold(stations, tol):
             statlist.append((i, i.relative_water_level()))
     statlist.sort(key = lambda x: x[1], reverse=True) #sorts list by 2nd element (relative water level)
     return statlist
+
+def stations_highest_rel_level(stations, N):
+    """"returns a list of the N stations with highest relative water levels"""
+    statlist = []                                                #
+    for i in stations:                                           #
+        if i.relative_water_level() == None:                     #
+            next                                                 # could replace with stations_level_over_threshold with tol = small no. (-ve)
+        else:                                                    #  this way feels more correct
+            statlist.append((i, i.relative_water_level()))       #
+    statlist.sort(key = lambda x: x[1], reverse=True)            #
+    Nlist =[]
+    for i in range(0, N):
+        Nlist.append(statlist[i][0])                         #appends ith term from 0-N for statlist, but only the 0th element (MonitoringStation object)
+    return Nlist
--- test_flood.py
+++ test_flood.py
@@ -1,7 +1,7 @@
 """Unit test for flood module."""
 
 from floodsystem.stationdata import build_station_list
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
 from floodsystem.station import MonitoringStation
 
 def test_stations_level_over_threshold():
@@ -21,3 +21,19 @@ def test_stations_level_over_threshold():
     assert type(reslist[0]) == tuple #checking type of list element
     assert reslist[0] == (testB, 5) #checking output list is sorted, and gives right output
     assert len(reslist) == 3 #checking that testD & E are removed to give length of 3
+
+def test_stations_highest_rel_level():
+    testA = MonitoringStation("A","","","",[1,2],"","")
+    testB = MonitoringStation("B","","","",[1,2],"","")  
+    testC = MonitoringStation("C","","","",[1,2],"","")  
+    testD = MonitoringStation("D","","","",[1,2],"","")  
+    testA.latest_level = 10
+    testB.latest_level = 8 
+    testC.latest_level = 6 
+    testD.latest_level = 4
+    stations = [testA,testB,testC,testD]
+    output = stations_highest_rel_level(stations, 3)
+    print(output)
+    assert len(output) == 3 #checks only 3 are in list
+    assert output[0] == testA #checks first in list is greatest rel level
+    assert output[2] == testC #checks last in list smallest rel level within N
