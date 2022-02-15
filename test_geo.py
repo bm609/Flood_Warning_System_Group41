9553d4884698ef4e1229e7d640094bd8566c17bb
Joecam2022 <98177868+Joecam2022@users.noreply.github.com>
2022-02-15 10:00:15 +0000

    Update test_geo.py

--- test_geo.py
+++ test_geo.py
@@ -34,21 +34,20 @@ from floodsystem.geo import stations_within_radius
 def test_stations_within_radius():
     coord=[(0,0),(0.01,0.001),(3,0),(0,0.001)]
     
-    from haversine import haversine
-    
+        
     # 0:ID, 1:ID, 2:NAME, 3:COORDINATE 4:TYPICAL RANGE, 5:RIVER, 6:TOWN
     stationA = MonitoringStation("TestA0", "TestA1", "TestA2", coord[0], "TestA4", "TestA5","TestA6")
     stationB = MonitoringStation("TestB0", "TestB1", "TestB2", coord[1], "TestB4", "TestB5","TestB6")
     stationC = MonitoringStation("TestC0", "TestC1", "TestC2", coord[2], "TestC4", "TestC5","TestC6")
     stationD = MonitoringStation("TestD0", "TestD1", "TestD2", coord[3], "TestD4", "TestD5","TestD6")
     stations_test=[stationA,stationB,stationC,stationD]
+    
     centre = (0,0)
-    print()
     r=10
     output=stations_within_radius(stations_test, centre, r)
+    # check the type of output
     assert type(output) == list
-    print(output)
-    #Names are Test_n_2 variables
+    # check the output is correct for r=10
     assert output[0][0]=='TestA2'
     assert output[1][0]=='TestD2'
     assert output[2][0]=='TestB2'
@@ -65,7 +64,23 @@ def test_rivers_by_station_number():
     stationB = MonitoringStation("TestB0", "TestB1", "TestB2","TestB3", "TestB4", "River 1","TestB6")
     stationC = MonitoringStation("TestC0", "TestC1", "TestC2","TestC3", "TestC4", "River 0","TestC6")
     stationD = MonitoringStation("TestD0", "TestD1", "TestD2","TestD3", "TestD4", "River 2","TestD6")
-    stations_test=[stationA,stationB,stationC,stationD]
+    stationE = MonitoringStation("TestE0", "TestE1", "TestE2","TestE3", "TestE4", "River 3","TestE6")
+    stationF = MonitoringStation("TestF0", "TestF1", "TestF2","TestF3", "TestF4", "River 3","TestF6")
+    stations_test=[stationA,stationB,stationC,stationD,stationE,stationF]
+    
     N=2
-    output=rivers_by_station_number(stations_test, N)
-    assert output==[('River 1', 2), ('River 2', 1)]
+    output,n=rivers_by_station_number(stations_test, N)
+    # check the output is correct for N=2
+    assert output==[('River 3', 2), ('River 1', 2)]
+    assert n==2
+    # check the type of output    
+    assert type(output)==list    
+    for i in range(N):
+        assert type(output[i][0])==str
+        assert type(output[i][1])==int
+
+#test 1C,1E
+#test_stations_within_radius()
+#test_rivers_by_station_number()
+
+
