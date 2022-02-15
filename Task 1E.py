69dfb290e03a0e4f400b91e010562e36fad0ada7
Joecam2022 <98177868+Joecam2022@users.noreply.github.com>
2022-02-15 10:01:01 +0000

    Update Task 1E.py

--- Task 1E.py
+++ Task 1E.py
@@ -5,11 +5,13 @@ def run():
     """Requirements for Task E"""
     stations = build_station_list() # list of stations
     number=9
-    answer=rivers_by_station_number(stations,number)
+    answer,no_of_data=rivers_by_station_number(stations,number)
     
-    
+    print('{} data extracted'.format(no_of_data))
+    if no_of_data>number:
+        print('Extra entries presented due to repeated values')
     print(answer)
     
-run()
+run() 
 
    # return output
