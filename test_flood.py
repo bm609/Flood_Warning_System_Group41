d38ee1595e728a501f6c13661e5b0282fbd5c188
bm609 <bm609@cam.ac.uk>
2022-03-01 12:22:36 +0000

    Added testing for graph plotting in plot_water_levels

--- test_flood.py
+++ test_flood.py
@@ -1,9 +1,10 @@
 """Unit test for flood module."""
 
+from matplotlib import dates
 from floodsystem.stationdata import build_station_list
-from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.flood import plot_water_levels, stations_level_over_threshold, stations_highest_rel_level
 from floodsystem.station import MonitoringStation
-
+import numpy as np
 def test_stations_level_over_threshold():
     testA = MonitoringStation("A","","","",[1,2],"","")
     testB = MonitoringStation("B","","","",[1,2],"","")  
@@ -23,6 +24,7 @@ def test_stations_level_over_threshold():
     assert len(reslist) == 3 #checking that testD & E are removed to give length of 3
 
 def test_stations_highest_rel_level():
+   
     testA = MonitoringStation("A","","","",[1,2],"","")
     testB = MonitoringStation("B","","","",[1,2],"","")  
     testC = MonitoringStation("C","","","",[1,2],"","")  
@@ -37,6 +39,17 @@ def test_stations_highest_rel_level():
     assert len(output) == 3 #checks only 3 are in list
     assert output[0] == testA #checks first in list is greatest rel level
     assert output[2] == testC #checks last in list smallest rel level within N
-#def test_plot_water_levels():
+
+def test_plot_water_levels():
+    import datetime as DT
+    import matplotlib.dates as mdates
+    import matplotlib.ticker as ticker
+    testA = MonitoringStation("A","A","A","A",[1,2],"A","A")
+    levels= np.random.randint(1,5,15)
+    print(levels)
+    dates = mdates.num2date(mdates.drange(DT.datetime(2022, 11, 30),
+                                      DT.datetime(2022, 12, 15),
+                                      DT.timedelta(days=1)))
+    plot_water_levels(testA, dates, levels)
 #seems difficult to test, as output is graph
-#mostly just dependent on input data
+#this test just shows the plotting works
